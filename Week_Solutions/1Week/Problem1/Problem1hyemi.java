a41a40866e9ad278efa0e5be8806ff2be09ae589
정혜미 <hyemi8292@naver.com>
2025-03-25 16:55:34 +0900

    Add

---
+++ Week_Solutions/1Week/Problem1/Problem1hyemi.java
@@ -0,0 +1,23 @@
+// Practice2
+// 배열 arr 에서 target 에 해당하는 값의 인덱스를 출력
+// 해당 값이 여러 개인 경우 가장 큰 인덱스 출력
+
+// 입출력 예시)
+// 배열 arr: 1, 1, 100, 1, 1, 1, 100
+// 결과: 6
+
+public class Practice2 {
+    public static void main(String[] args) {
+        int [] arr = {1, 1, 100, 1, 1, 1, 100};
+        int targetIdx=-1;
+        int data=100;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==data){
+                if(i>targetIdx)
+                    targetIdx=i;
+            }
+        }
+        if(targetIdx>-1)
+            System.out.println("결과: "+targetIdx);
+    }
+}
