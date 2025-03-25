6917312fc3c7d4c9d9cafb2172f3861d20974ef9
hyemi0622 <hyemi8292@naver.com>
2025-03-25 22:00:19 +0900

    Delete test1.py

--- test1.py
+++
@@ -1,8 +0,0 @@
-def solution(arr, n):
-    if len(arr) % 2== 1:
-         return [arr[i] + n if i % 2 == 0 else arr[i] for i in range(len(arr))]
-    else:
-        return [arr[i]+ n if i % 2 ==1 else arr[i] for i in range(len(arr))]
-
-print(solution([49, 12, 100, 276, 33], 27))
-print(solution([444, 555, 666, 777], 100))
