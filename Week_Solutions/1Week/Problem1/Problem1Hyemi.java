fcc4d15ccc6ecfb0dccd27af8e23792ea68913c0
hyemi0622 <hyemi8292@naver.com>
2025-03-25 23:06:21 +0900

    Update Problem1Hyemi.java

--- Week_Solutions/1Week/Problem1/Problem1Hyemi.java
+++ Week_Solutions/1Week/Problem1/Problem1Hyemi.java
@@ -4,10 +4,9 @@ public class Problem1Hyemi {
         if(arr.length%2!=0)
             for(int i=0;i<arr.length;i++)
                 arr[i]=(i%2==0)?arr[i]+n:arr[i];                 
-        else{
+        else
             for(int i=0;i<arr.length;i++)
                 arr[i]=(i%2!=0)?arr[i]+n:arr[i];
-        }
+        
         return arr;
-    }
 }
