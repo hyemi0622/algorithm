2b89e9ca7058cc608b13222ce1159f5769abec2c
hyemi0622 <hyemi8292@naver.com>
2025-03-30 15:46:37 +0900

    Delete Week_Solutions/1Week/Problem1/Problem1bin.py

--- Week_Solutions/1Week/Problem1/Problem1bin.py
+++
@@ -1,9 +0,0 @@
-def solution(numbers, direction):
-    if direction == "right":
-        numbers = [numbers[-1]] + numbers[:-1]
-        return numbers
-    elif direction == "left":
-        numbers = [numbers[1]] + numbers[2:] + [numbers[0]]
-        return numbers
-print(solution([1,2,3], "right"))
-print(solution([4, 455, 6, 4, -1, 45, 6], "left"))
