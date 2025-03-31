3575c8e3d5b22acbdbb1cb13af267e5b8af0eabb
Stella <133227322+stellalee1210@users.noreply.github.com>
2025-03-31 20:38:40 +0900

    Create Problem3yeobin.js

---
+++ Week_Solutions/1Week/Problem3/Problem3yeobin.js
@@ -0,0 +1,7 @@
+//이여빈
+
+function solution(l, r) {
+    let arr = Array.from({length:r-l+1}, (v,i)=>i+l)
+    arr = arr.filter(v=> v.toString().replace(/[0,5]/g, '') === '')
+    return arr.length === 0 ? [-1] : arr;
+}
