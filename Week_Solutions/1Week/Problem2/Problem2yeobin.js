9aafd44da16c302f163d05ff868b37a08cc2748f
Stella <133227322+stellalee1210@users.noreply.github.com>
2025-03-31 20:13:13 +0900

    Create Problem2yeobin.js

---
+++ Week_Solutions/1Week/Problem2/Problem2yeobin.js
@@ -0,0 +1,14 @@
+//이여빈
+
+function solution(numbers, direction) {
+    
+    if(direction === "left"){
+        const temp = numbers.shift()
+        numbers.push(temp)
+    }else if(direction === "right"){
+        const temp = numbers.pop()
+        numbers.unshift(temp)
+    }
+    
+    return numbers;
+}
