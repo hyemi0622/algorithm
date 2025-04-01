8d9574b81edf74af67a7a2517c3ac84879eff519
dohun7050 <dohun3206@gmail.com>
2025-04-01 20:25:51 +0900

    Create Problem3dohun.cpp

---
+++ Week_Solutions/2Week/Problem3/Problem3dohun.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include<list>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(long long n) {
+    list<int> a;
+    for (int i = 1; i < a.size(); ++i) 
+        a.push_back(i);
+
+    a.reverse();
+    for (auto i : a)
+    {
+        cout<<
+    }
+
+    
+}
