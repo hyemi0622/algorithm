567d3e431851a604ac3aaa478513da3d43b3e337
hyemi0622 <hyemi8292@naver.com>
2025-04-04 23:28:16 +0900

    Update LinkedListPrac.py

--- Practice!/Week2/LinkedListPrac.py
+++ Practice!/Week2/LinkedListPrac.py
@@ -59,5 +59,75 @@ linked_list.show_data()
 
 result = remove_dup(linked_list)
 
+
+
+# Practice2
+# Palindrome 연결 리스트
+# 추가 자료구조 없이 연결 리스트만으로 풀어보기 Practice1에서 만든 단일연결리스트 사용(내장 LinkedList말고)
+# Palindrome: 앞으로 읽어도 뒤로 읽어도 같은 문자열
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def add_data(self, data):
+        if not self.head:
+            self.head = Node(data)
+        else:
+            cur = self.head
+            while cur.next:
+                cur = cur.next
+            cur.next = Node(data)
+
+    def find_data(self, data):
+        cur = self.head
+        while cur:
+            if cur.data == data:
+                return True
+            cur = cur.next
+        return False
+
+    def show_data(self):
+        cur = self.head
+        while cur:
+            print(cur.data, end=" ")
+            cur = cur.next
+        print()
+
+    def is_palindrome(self):
+        //여기에 코드 작성
+        return false
+
+    
+
+
+# Test code
+linked_list1 = LinkedList()
+linked_list1.add_data(1)
+linked_list1.add_data(3)
+linked_list1.add_data(5)
+linked_list1.add_data(3)
+linked_list1.add_data(1)
+print(linked_list1.is_palindrome())  # Output: True
+
+linked_list2 = LinkedList()
+linked_list2.add_data(3)
+linked_list2.add_data(5)
+linked_list2.add_data(5)
+linked_list2.add_data(3)
+print(linked_list2.is_palindrome())  # Output: True
+
+linked_list3 = LinkedList()
+linked_list3.add_data(1)
+linked_list3.add_data(3)
+linked_list3.add_data(5)
+linked_list3.add_data(1)
+print(linked_list3.is_palindrome())  # Output: False
+
+
 print("After removing duplicates:")
 result.show_data()
