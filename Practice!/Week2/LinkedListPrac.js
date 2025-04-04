90fa786e9c97e0933e70d585192b76fa5589db71
hyemi0622 <hyemi8292@naver.com>
2025-04-04 23:24:28 +0900

    Update LinkedListPrac.js

--- Practice!/Week2/LinkedListPrac.js
+++ Practice!/Week2/LinkedListPrac.js
@@ -73,3 +73,84 @@ const result = removeDup(linkedList);
 
 console.log("After removing duplicates:");
 result.showData();
+
+
+
+// Practice2
+// Palindrome 연결 리스트
+// 추가 자료구조 없이 연결 리스트만으로 풀어보기 Practice1에서 만든 단일연결리스트 사용(내장 LinkedList말고)
+// Palindrome: 앞으로 읽어도 뒤로 읽어도 같은 문자열
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  addData(data) {
+    if (!this.head) {
+      this.head = new Node(data);
+      return;
+    }
+
+    let current = this.head;
+    while (current.next) {
+      current = current.next;
+    }
+    current.next = new Node(data);
+  }
+
+  findData(data) {
+    let current = this.head;
+    while (current) {
+      if (current.data === data) return true;
+      current = current.next;
+    }
+    return false;
+  }
+
+  showData() {
+    let current = this.head;
+    const result = [];
+    while (current) {
+      result.push(current.data);
+      current = current.next;
+    }
+    console.log(result.join(" "));
+  }
+}
+
+
+function checkPalindrome(linkedList) {
+   //여기에 코드 작성
+  return false;
+}
+
+// Test code
+const linkedList1 = new LinkedList();
+linkedList1.addData(1);
+linkedList1.addData(3);
+linkedList1.addData(5);
+linkedList1.addData(3);
+linkedList1.addData(1);
+console.log(checkPalindrome(linkedList1)); // true
+
+const linkedList2 = new LinkedList();
+linkedList2.addData(3);
+linkedList2.addData(5);
+linkedList2.addData(5);
+linkedList2.addData(3);
+console.log(checkPalindrome(linkedList2)); // true
+
+const linkedList3 = new LinkedList();
+linkedList3.addData(1);
+linkedList3.addData(3);
+linkedList3.addData(5);
+linkedList3.addData(1);
+console.log(checkPalindrome(linkedList3)); // false
+
