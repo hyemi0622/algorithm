9040c13900089306cf344076c1af423135ae23d3
hyemi0622 <hyemi8292@naver.com>
2025-04-04 23:22:38 +0900

    Update LinkedListPrac.cpp

--- Practice!/Week2/LinkedListPrac.cpp
+++ Practice!/Week2/LinkedListPrac.cpp
@@ -89,3 +89,95 @@ int main() {
 }
 
 
+// Practice2
+// Palindrome 연결 리스트
+// 추가 자료구조 없이 연결 리스트만으로 풀어보기 Practice1에서 만든 단일연결리스트 사용(내장 LinkedList말고)
+// Palindrome: 앞으로 읽어도 뒤로 읽어도 같은 문자열
+
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int data) {
+        this->data = data;
+        this->next = nullptr;
+    }
+};
+
+class LinkedList {
+public:
+    Node* head;
+
+    LinkedList() {
+        this->head = nullptr;
+    }
+
+    void addData(int data) {
+        if (this->head == nullptr) {
+            this->head = new Node(data);
+        } else {
+            Node* cur = this->head;
+            while (cur->next != nullptr) {
+                cur = cur->next;
+            }
+            cur->next = new Node(data);
+        }
+    }
+
+    bool findData(int data) {
+        Node* cur = this->head;
+        while (cur != nullptr) {
+            if (cur->data == data) {
+                return true;
+            }
+            cur = cur->next;
+        }
+        return false;
+    }
+
+    void showData() {
+        Node* cur = this->head;
+        while (cur != nullptr) {
+            cout << cur->data << " ";
+            cur = cur->next;
+        }
+        cout << endl;
+    }
+};
+
+bool checkPalindrome(LinkedList& list) {
+    //여기에 코드 작
+    return true;
+}
+
+int main() {
+    LinkedList linkedList1;
+    linkedList1.addData(1);
+    linkedList1.addData(3);
+    linkedList1.addData(5);
+    linkedList1.addData(3);
+    linkedList1.addData(1);
+    cout << checkPalindrome(linkedList1) << endl; // true
+
+    LinkedList linkedList2;
+    linkedList2.addData(3);
+    linkedList2.addData(5);
+    linkedList2.addData(5);
+    linkedList2.addData(3);
+    cout << checkPalindrome(linkedList2) << endl; // true
+
+    LinkedList linkedList3;
+    linkedList3.addData(1);
+    linkedList3.addData(3);
+    linkedList3.addData(5);
+    linkedList3.addData(1);
+    cout << checkPalindrome(linkedList3) << endl; // false
+
+    return 0;
+}
+
