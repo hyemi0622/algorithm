b13a3b346b97878dd9cf52c8a9450ffd45b08d50
hyemi0622 <hyemi8292@naver.com>
2025-04-04 23:19:37 +0900

    Update LinkedListPrac.java

--- Practice!/Week2/LinkedListPrac.java
+++ Practice!/Week2/LinkedListPrac.java
@@ -121,3 +121,54 @@ public class Practice1 {
     }
 }
 
+
+
+// Practice2
+// Palindrome 연결 리스트
+// 추가 자료구조 없이 연결 리스트만으로 풀어보기 Practice1에서 만든 단일연결리스트 사용(내장 LinkedList말고)
+// Palindrome: 앞으로 읽어도 뒤로 읽어도 같은 문자열
+
+// 입력 예시)
+// 입력 연결 리스트: 1, 3, 5, 3, 1
+// 결과: true
+
+// 입력 연결 리스트: 3, 5, 5, 3
+// 결과: true
+
+// 입력 연결 리스트: 1, 3, 5, 1
+// 결과: false
+
+
+public class Practice {
+    public static boolean checkPalindrome(LinkedList list) {
+        //여기에 코드 작성
+        return false;
+    }
+
+    public static void main(String[] args) {
+        // Test code
+        LinkedList linkedList = new LinkedList();
+        linkedList.addData(1);
+        linkedList.addData(3);
+        linkedList.addData(5);
+        linkedList.addData(3);
+        linkedList.addData(1);
+        System.out.println(checkPalindrome(linkedList));
+
+        LinkedList linkedList2 = new LinkedList();
+        linkedList2.addData(3);
+        linkedList2.addData(5);
+        linkedList2.addData(5);
+        linkedList2.addData(3);
+        System.out.println(checkPalindrome(linkedList2));
+
+        LinkedList linkedList3 = new LinkedList();
+        linkedList3.addData(1);
+        linkedList3.addData(3);
+        linkedList3.addData(5);
+        linkedList3.addData(1);
+        System.out.println(checkPalindrome(linkedList3));
+
+    }
+}
+
