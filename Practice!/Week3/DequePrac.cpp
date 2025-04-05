38d0754a6ded41fdbce991b91432354db18eb643
hyemi0622 <hyemi8292@naver.com>
2025-04-05 18:31:29 +0900

    Update DequePrac.cpp

--- Practice!/Week3/DequePrac.cpp
+++ Practice!/Week3/DequePrac.cpp
@@ -6,3 +6,22 @@
 // 입력 예시)
 // 입력 데이터: 1 -> 2 -> 3 -> 4 -> 5
 // 출력 데이터: 1 -> 5 -> 2 -> 4 -> 3
+#include <iostream>
+#include <deque>
+using namespace std;
+
+void reorderData(int arr[], int n) {
+    deque<int> dq;
+    //여기에 코드 작성
+}
+
+int main() {
+    // Test code
+    int arr1[] = {1, 2, 3, 4, 5};
+    reorderData(arr1, 5); // 출력: 1 -> 5 -> 2 -> 4 -> 3
+
+    int arr2[] = {1, 2, 3, 4, 5, 6, 7};
+    reorderData(arr2, 7); // 출력: 1 -> 7 -> 2 -> 6 -> 3 -> 5 -> 4
+
+    return 0;
+}
