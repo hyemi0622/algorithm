3ee8a5754cebcb28ed46a3bd989db1cd581c52fa
hyemi0622 <hyemi8292@naver.com>
2025-04-05 18:29:53 +0900

    Update DequePrac.java

--- Practice!/Week3/DequePrac.java
+++ Practice!/Week3/DequePrac.java
@@ -1,1 +1,28 @@
+// Practice1
+// 데이터 재정렬
+// D_0 -> D_1 -> ... -> D_n-1 -> D_n 순으로 되어 있는 데이터를
+// D_0 -> D_n -> D_1 -> D_n-1 -> ... 순이 되도록 재정렬 하시오.
+
+// 입력 예시)
+// 입력 데이터: 1 -> 2 -> 3 -> 4 -> 5
+// 출력 데이터: 1 -> 5 -> 2 -> 4 -> 3
+
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Practice1 {
+    public static void reorderData(int[] arr) {
+        //여기에 코드 작성
+    }
+
+    public static void main(String[] args) {
+        // Test code
+        int[] arr = {1, 2, 3, 4, 5};
+        reorderData(arr);   // 1 -> 5 -> 2 -> 4 -> 3
+
+        int[] arr2 = {1, 2, 3, 4, 5, 6, 7};
+        reorderData(arr2);  // 1 -> 7 -> 2 -> 6 -> 3 -> 5 -> 4
+    }
+}
 
