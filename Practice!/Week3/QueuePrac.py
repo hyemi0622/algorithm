6fdda98046a3d60a237e8755e412deebed29430d
hyemi0622 <hyemi8292@naver.com>
2025-04-05 18:05:39 +0900

    Create QueuePrac.py

---
+++ Practice!/Week3/QueuePrac.py
@@ -0,0 +1,18 @@
+# Practice1
+# 카드 섞기
+# 1부터 N 까지의 번호로 구성된 N장의 카드가 있다.
+# 1번 카드가 가장 위에 그리고 N번 카드는 가장 아래의 상태로 카드가 순서대로 쌓여있다.
+# 아래의 동작을 카드 한 장만 남을 때까지 반복했을 때, 가장 마지막 남는 카드 번호를 출력하시오.
+# 1. 가장 위의 카드는 버린다.
+# 2. 그 다음 위의 카드는 쌓여 있는 카드의 가장 아래에 다시 넣는다.
+from collections import deque
+
+def findLastCard(N):
+  //여기에 코드 작성  
+  return null;
+
+# Test code
+print(findLastCard(4)) # 출력: 4
+print(findLastCard(7)) # 출력: 6
+print(findLastCard(9)) # 출력: 2
+ 
