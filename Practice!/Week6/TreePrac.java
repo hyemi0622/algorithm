d9dc12b4fcf9a5c60cf8232d483a8fff17c19d63
hyemi0622 <hyemi8292@naver.com>
2025-04-29 10:19:42 +0900

    Update TreePrac.java

--- Practice!/Week6/TreePrac.java
+++ Practice!/Week6/TreePrac.java
@@ -13,3 +13,43 @@
 
 // 입력: 3
 // 출력: 0, 0, 1, 0, 0, 1, 1
+public class Practice1 {
+    public static void solution(int n) {
+        //작성 (함수 더 만들어두 됨)
+    }
+
+    
+    public static void main(String[] args) {
+        // Test code
+        solution(1);
+        solution(2);
+        solution(3);
+    }
+}
+
+
+
+
+
+// Practice2
+// 각각의 에지에 가중치가 있는 포화 이진 트리가 있다.
+// 루트에서 각 리프까지의 경로 길이를 모두 같게 설정하고,
+// 이 때, 모든 가중치들의 총합이 최소가 되도록 하는 프로그램을 작성하세요.
+
+public class Practice2 {
+    public static void solution(int h, int[] w) {
+        //작성 (함수 더 만들어두 됨)
+    }
+    
+    public static void main(String[] args) {
+        // Test code
+        int h = 2;
+        int[] w = {2, 2, 2, 1, 1, 3};
+        solution(h, w);
+        System.out.println();
+
+        h = 3;
+        w = new int[]{1, 2, 1, 3, 2, 4, 1, 1, 1, 1, 1, 1, 1, 1};
+        solution(h, w);
+    }
+}
