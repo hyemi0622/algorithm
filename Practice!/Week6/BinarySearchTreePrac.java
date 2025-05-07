445da9e503fdd5de07f2f1414e1404a833a38ce2
hyemi0622 <hyemi8292@naver.com>
2025-05-07 20:11:58 +0900

    Update BinarySearchTreePrac.java

--- Practice!/Week6/BinarySearchTreePrac.java
+++ Practice!/Week6/BinarySearchTreePrac.java
@@ -67,3 +67,36 @@ public class Practice1 {
         solution(data, n);
     }
 }
+
+
+// Practice2
+// 주어진 BST 에서 노드 간의 차이값 중 최소 값을 구하세요.
+
+// 입력 트리: 4, 2, 6, 1, 3
+// 출력: 1
+
+// 입력 트리: 5, 1, 48, null, null, 12, 51
+// 출력: 3
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Practice2 {
+
+    public static void solution(Integer[] data) {
+       //코드 작성 필요시 함수 더 추가 가능
+
+    }
+
+
+    public static void main(String[] args) {
+        // Test code
+        Integer[] data = {3, 1, 4, null, 2};
+        solution(data);
+
+        data = new Integer[]{5, 1, 48, null, null, 12, 51};
+        solution(data);
+    }
+}
