514cf2009e3ad52a25f2bd5588c0b77eca4e667e
hyemi0622 <hyemi8292@naver.com>
2025-05-08 21:17:35 +0900

    Update hyemiproblem1.java

--- Week_Solutions/6Week/Problem1/hyemiproblem1.java
+++ Week_Solutions/6Week/Problem1/hyemiproblem1.java
@@ -1,1 +1,25 @@
-
+import java.util.*;
+class Solution {
+    public String solution(String s) {
+        HashMap<Character,Integer>map = new HashMap<>();
+        for(char item : s.toCharArray()){
+            if(map.containsKey(item)){
+                map.put(item,map.get(item)+1);
+            } else{
+                map.put(item,1);
+            }
+        }
+        List list = new ArrayList();
+        for(char set : map.keySet()){
+            if(map.get(set)==1){
+                list.add(set);
+            }
+        }
+        Collections.sort(list);
+        String result="";
+        for (int i = 0; i < list.size(); i++) {
+            result+=list.get(i);
+        }
+        return result;
+    }
+}
