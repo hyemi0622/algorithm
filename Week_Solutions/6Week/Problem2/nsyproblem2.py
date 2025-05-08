7b66c70fd1f4d2163fee37f703969accc4e15079
Nemo S. Yelan <asdqazsa@gmail.com>
2025-05-08 21:13:00 +0900

    Update nsyproblem2.py
    asdf

--- Week_Solutions/6Week/Problem2/nsyproblem2.py
+++ Week_Solutions/6Week/Problem2/nsyproblem2.py
@@ -6,3 +6,27 @@ def solution(before, after):
         else:
             answer = 0
     return answer
+
+######################################################################
+class letters:
+    def __init__(self):
+        self.count = 1
+        self.dict = {}
+
+    def incheck(self, letter):
+        return letter in self.dict.keys
+    
+    def division(self, letter):
+        if not self.incheck(letter):
+            self.dict[letter] = 1
+        else:
+            self.dict[letter] += 1
+
+def solution(before, after):
+    bb = letters()
+    ff = letters()
+    for i in before:
+        bb.division(i)
+    for i in after:
+        ff.division(i)
+    return bb == ff
