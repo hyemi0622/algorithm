20cfa1dc20a789a31e5030cdf2d87faf92417895
Nemo S. Yelan <asdqazsa@gmail.com>
2025-05-08 21:10:03 +0900

    Update nsyproblem1.py
    asdf

--- Week_Solutions/6Week/Problem1/nsyproblem1.py
+++ Week_Solutions/6Week/Problem1/nsyproblem1.py
@@ -5,3 +5,30 @@ def solution(s):
             letters += letter
     answer = ''.join(sorted(letters))
     return answer
+
+####################################################################
+class letters:
+    def __init__(self):
+        self.count = 1
+        self.dict = {}
+
+    def incheck(self, letter):
+        return letter in self.dict.keys
+    
+    def division(self, letter):
+        if not self.incheck(letter):
+            self.dict[letter] = 1
+        else:
+            self.dict[letter] += 1
+
+def solution(s):
+    solve = letters()
+    answer = ''
+    for i in s:
+        solve.division(i)
+    for i in solve.dict.keys():
+        if solve.dict[i] == 1:
+            answer + i
+
+    answer = ''.join(sorted(answer))
+    return answer
