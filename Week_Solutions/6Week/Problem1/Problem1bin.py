6fc7b41a38e4fd75bf5802ea060f6e4b525e3e5e
김정빈 <kbjn981@gmail.com>
2025-05-09 19:52:18 +0900

    Add Problem1 Solution kim bin

---
+++ Week_Solutions/6Week/Problem1/Problem1bin.py
@@ -0,0 +1,9 @@
+from collections import Counter
+def solution(s):
+    count = Counter(s)
+    unique_chars = [char for char in count if count[char] == 1]
+    unique_chars.sort()
+    return ''.join(unique_chars)
+print(solution("abcabcadc")) 
+print(solution("abdc"))       
+print(solution("aabbcc"))
