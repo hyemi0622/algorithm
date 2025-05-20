2e79a44d93426d539c475652a2c5b588932edabe
hyemi0622 <hyemi8292@naver.com>
2025-05-20 20:04:06 +0900

    Update Problem1hyemi.java

--- Week_Solutions/8Week/Problem1/Problem1hyemi.java
+++ Week_Solutions/8Week/Problem1/Problem1hyemi.java
@@ -1,1 +1,21 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        PriorityQueue<Integer>pq = new PriorityQueue<>();
+        for(int num : scoville){
+            pq.offer(num);
+        }
 
+        while(pq.peek()<K && pq.size()>=2) {
+            int small1 = pq.poll();
+            int small2 = pq.poll();
+            pq.offer(small1+(small2*2));
+            answer++;
+            if(pq.peek()<K && pq.size()<2){
+                answer=-1;
+            }
+        }
+        return answer;
+    }
+}
