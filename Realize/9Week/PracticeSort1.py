ef3c37d7ba6bbd15bd3018f54825087da4108a85
hyemi0622 <hyemi8292@naver.com>
2025-05-26 17:13:09 +0900

    Update PracticeSort1.py

--- Realize/9Week/PracticeSort1.py
+++ Realize/9Week/PracticeSort1.py
@@ -1,9 +1,10 @@
 def bubble_sort(arr):
+     # 1.
     for i in range(1, len(arr) - 1):
         for j in range(len(arr) - i):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
-
+    # 2. (1과 2 같은 방식인데 i 인덱스 사용 다르게 참고)
     for i in range(len(arr) - 1, 0, -1):
         for j in range(i):
             if arr[j] > arr[j + 1]:
@@ -18,13 +19,14 @@ def insertion_sort(arr):
                 break
 
 def selection_sort(arr):
+     # 1. 최소 값을 찾아 앞 쪽부터 교환하는 방식
     for i in range(len(arr) - 1):
         min_idx = i
         for j in range(i + 1, len(arr)):
             if arr[j] < arr[min_idx]:
                 min_idx = j
         arr[i], arr[min_idx] = arr[min_idx], arr[i]
-
+     # 2. 최대 값을 찾아 뒤 쪽부터 교환하는 방식
     for i in range(len(arr) - 1, 0, -1):
         max_idx = i
         for j in range(i - 1, -1, -1):
