bdf1aa88c2a0dc86714b757331f98b2c8511bed7
hyemi0622 <hyemi8292@naver.com>
2025-05-27 21:12:44 +0900

    Update Problem1Hyemi.java

--- Week_Solutions/9Week/Problem1/Problem1Hyemi.java
+++ Week_Solutions/9Week/Problem1/Problem1Hyemi.java
@@ -1,1 +1,20 @@
-
+class Solution {
+    public long solution(long n) {
+        long answer = 0;
+        String str = Long.toString(n);
+        char [] c = str.toCharArray();
+        for(int i=1;i<c.length;i++){
+            for (int j = i; j > 0 ; j--) {
+                if(c[j]>c[j-1]){
+                    char temp=c[j];
+                    c[j]=c[j-1];
+                    c[j-1]=temp;
+                }
+            }
+        }
+        for (int i = 0; i < c.length; i++) {
+            answer=answer*10+(int)(c[i]-48);
+        }
+        return answer;
+    }
+}
