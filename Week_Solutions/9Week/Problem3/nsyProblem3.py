a8633a9fa0df4537fc0d104856d33e3b3f6ed9ee
Nemo S. Yelan <asdqazsa@gmail.com>
2025-05-27 20:51:33 +0900

    Update nsyProblem3.py
    asdf

--- Week_Solutions/9Week/Problem3/nsyProblem3.py
+++ Week_Solutions/9Week/Problem3/nsyProblem3.py
@@ -8,3 +8,19 @@ def solution(strings, n):
                     strings[j], strings[j + 1] = strings[j + 1], strings[j]
     answer = strings
     return answer
+
+#################################################################################################
+def bubble(arr, n):
+    for i in range(len(arr) -1):
+        for j in range(len(arr) -1 -i):
+            if ord(arr[j][n]) > ord(arr[j + 1][n]):
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+            elif ord(arr[j][n]) == ord(arr[j + 1][n]):
+                for k in range(len(arr[j])):
+                    if ord(arr[j][k]) > ord(arr[j + 1][k]):
+                        arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+
+def solution(strings, n):
+    answer = bubble(strings, n)
+    return answer
